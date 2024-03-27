36bd316656c0286aa1f599ac564f31bc50695eca
ytakasugi <sh7.tibi0129@gmail.com>
2024-03-27 23:27:53 +0900

    change file

--- chapter01/socket-programming/src/main.rs
+++ chapter01/socket-programming/src/main.rs
@@ -11,16 +11,13 @@ fn main() {
     env::set_var("RUST_LOG", "debug");
     env_logger::init();
     let args: Vec<String> = env::args().collect();
-
     if args.len() != 4 {
         error!("Please specify [tcp|udp] [server|client] [addr:port].");
         std::process::exit(1);
     }
-
     let protocol: &str = &args[1];
     let role: &str = &args[2];
     let address = &args[3];
-
     match protocol {
         "tcp" => match role {
             "server" => {
@@ -35,23 +32,26 @@ fn main() {
         },
         "udp" => match role {
             "server" => {
-                todo!()
+                udp_server::serve(address).unwrap_or_else(|e| error!("{}", e));
             }
             "client" => {
-                todo!()
+                udp_client::communicate(address).unwrap_or_else(|e| error!("{}", e));
             }
             _ => {
                 missing_role();
             }
         },
         _ => {
-            error!("Please specify tcp or udp on the first argument.");
+            error!("Please specify tcp or udp on the 1st argument.");
             std::process::exit(1);
         }
     }
 }
 
+/**
+ * 第2引数が不正な時にエラーを出す関数
+ */
 fn missing_role() {
-    error!("Please specify [server|client].");
+    error!("Please specify server or client on the 2nd argument.");
     std::process::exit(1);
 }
--- chapter01/socket-programming/src/tcp_client.rs
+++ chapter01/socket-programming/src/tcp_client.rs
@@ -1,4 +1,3 @@
-
 use std::io::{self, BufRead, BufReader, Write};
 use std::net::TcpStream;
 use std::str;
@@ -6,10 +5,12 @@ use std::str;
 pub fn connect(address: &str) -> Result<(), failure::Error> {
     let mut stream = TcpStream::connect(address)?;
     loop {
+        // 入力データをソケットから送信。
         let mut input = String::new();
         io::stdin().read_line(&mut input)?;
         stream.write_all(input.as_bytes())?;
 
+        // ソケットから受信したデータを表示。
         let mut reader = BufReader::new(&stream);
         let mut buffer = Vec::new();
         reader.read_until(b'\n', &mut buffer)?;
